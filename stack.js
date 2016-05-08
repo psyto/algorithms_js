853e40e20c0c866b7864edd0a225d875da735f47
Hiroyuki Saito <psyhiro@yahoo.co.jp>
2016-05-08 20:40:13 +0900

    second commit

--- stack.js
+++ stack.js
@@ -6,8 +6,6 @@ function Stack() {
     this.push = push;
     this.pop = pop;
     this.peek = peek;
-    this.clear = clear;
-    this.length = length;
 }
 
 function push(element) {
@@ -22,27 +20,16 @@ function peek() {
     return this.dataStore[this.top-1];
 }
 
-function clear() {
-    this.top = 0;
-}
-
-function length() {
-    return this.top;
-}
-
 var s = new Stack();
 s.push("David");
 s.push("Raymond");
 s.push("Bryan");
-console.log("length: " + s.length());
 console.log(s.peek());
 var popped = s.pop();
 console.log("The popped element is: " + popped);
 console.log(s.peek());
 s.push("Cynthia");
 console.log(s.peek());
-s.clear();
-console.log("length: " + s.length());
 console.log(s.peek());
 s.push("Cryton");
 console.log(s.peek());
