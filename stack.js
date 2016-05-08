d7bb5d3d36cf6a005011ef90b9a1b79b22ac9f66
Hiroyuki Saito <psyhiro@yahoo.co.jp>
2016-05-08 19:46:57 +0900

    first commit

---
+++ stack.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+function Stack() {
+    this.dataStore = [];
+    this.top = 0;
+    this.push = push;
+    this.pop = pop;
+    this.peek = peek;
+    this.clear = clear;
+    this.length = length;
+}
+
+function push(element) {
+    this.dataStore[this.top++] = element;
+}
+
+function pop() {
+    return this.dataStore[--this.top];
+}
+
+function peek() {
+    return this.dataStore[this.top-1];
+}
+
+function clear() {
+    this.top = 0;
+}
+
+function length() {
+    return this.top;
+}
+
+var s = new Stack();
+s.push("David");
+s.push("Raymond");
+s.push("Bryan");
+console.log("length: " + s.length());
+console.log(s.peek());
+var popped = s.pop();
+console.log("The popped element is: " + popped);
+console.log(s.peek());
+s.push("Cynthia");
+console.log(s.peek());
+s.clear();
+console.log("length: " + s.length());
+console.log(s.peek());
+s.push("Cryton");
+console.log(s.peek());
+
