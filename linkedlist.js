a0aeb2f9b5f20781cf8f0a191373834f14369a22
Hiroyuki Saito <psyhiro@yahoo.co.jp>
2016-05-10 23:00:33 +0900

    first commit

---
+++ linkedlist.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+function Node(element) {
+    this.element = element;
+    this.next = null;
+}
+
+function LinkedList() {
+    this.head = new Node("head");
+    this.find = find;
+    this.insert = insert;
+    this.display = display;
+    this.findPrevious = findPrevious;
+    this.remove = remove;
+}
+
+function find(item) {
+    var currNode = this.head;
+    while (currNode != item) {
+        currNode = currNode.next;
+    }
+    return currNode;
+}
+
+function insert(newElement, item) {
+    var newNode = Node(newElement);
+    var current = this.find(item);
+    newNode.next = current.next;
+    current.next = newNode;
+}
+
+function display() {
+    var currNode = this.head;
+    while (!(currNode == null)) {
+        print(currNode.next.element);
+        currNode = currNode.next;
+    }
+}
+
+function findPrevious(item) {
+    var currNode = this.head;
+    while (!(currNode == null) && (currNode.next.element != item)) {
+        currNode = currNode.next;
+    }
+    return currNode;
+}
+
+function remove(item) {
+    var prevNode = this.findPrevious(item);
+    if (!(prevNode.next == null)) {
+        prevNode.next = prevNode.next.next;
+    }
+}
+
+var cities = new LinkedList();
+cities.insert("Tokyo", "head");
+cities.insert("Osaka", "Tokyo");
+cities.insert("Nagoya", "Osaka");
+cities.insert("Hokkaido", "Nagoya");
+cities.display();
+console.log();
+cities.remove("Nagoya");
+cities.display();
+
